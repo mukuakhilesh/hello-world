128ae7fd0aee1eee2214ef150c26a9ba4814f7bd
akhi64187 <akhi@github.com>
2019-01-17 20:03:03 +0530

    changed hello world msg

--- hello.cpp
+++ hello.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-  cout<<"Hello_world\n";
+  cout<<"Takla ka b'day aaya'\n";
   cout<<"Experimenting\n";
   cout<<"Expreiment @\n";
   cout<<"Final_Experiment\n";
